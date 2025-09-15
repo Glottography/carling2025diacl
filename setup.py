fd0fccee11dcb2677c4deb2cd43117ac56b43977
Robert Forkel <xrotwang@googlemail.com>
2025-09-15 21:10:43 +0200

    work

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+import json
+
+
+with open('metadata.json', encoding='utf-8') as fp:
+    metadata = json.load(fp)
+
+
+setup(
+    name='cldfbench_carling2025diacl',
+    description=metadata['title'],
+    license=metadata.get('license', ''),
+    url=metadata.get('url', ''),
+    py_modules=['cldfbench_carling2025diacl'],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        'cldfbench.dataset': [
+            'carling2025diacl=cldfbench_carling2025diacl:Dataset',
+        ]
+    },
+    install_requires=[
+        'pyglottography',
+    ],
+    extras_require={
+        'test': [
+            'pytest-cldf',
+        ],
+    },
+)
